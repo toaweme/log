57b5258f9fa66a8c674ddf17c372c4dc4909c475
Ignas Bernotas <ignas@iber.lt>
2025-03-17 03:14:32 +0200

    ref: move gin and desktop logging code to separate packages

--- log.go
+++ log.go
@@ -1,33 +1,38 @@
 package log
 
 import (
-	"context"
 	"log/slog"
 	"os"
-	"path/filepath"
-	"runtime"
 	"sync"
-	"time"
-	
-	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 var (
-	level  = new(slog.LevelVar)
+	Level  = new(slog.LevelVar)
 	Logger *slog.Logger
 	mu     sync.RWMutex
 )
 
-// Internal function to set the global logger
-func setLogger(newLogger *slog.Logger) {
+func init() {
+	Level.Set(slog.LevelDebug)
+	SetLogger(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: Level})))
+}
+
+func SetLogger(newLogger *slog.Logger) {
 	mu.Lock()
 	defer mu.Unlock()
 	Logger = newLogger
 }
 
-func init() {
-	level.Set(slog.LevelDebug)
-	setLogger(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: level})))
+func SetLevel(lvl slog.Level) {
+	mu.Lock()
+	defer mu.Unlock()
+	Level.Set(lvl)
+}
+
+func GetLevel() *slog.LevelVar {
+	mu.RLock()
+	defer mu.RUnlock()
+	return Level
 }
 
 var (
@@ -36,68 +41,3 @@ var (
 	Debug = func(msg string, args ...any) { Logger.Debug(msg, args...) }
 	Warn  = func(msg string, args ...any) { Logger.Warn(msg, args...) }
 )
-
-func SetLevel(lvl slog.Level) {
-	level.Set(lvl)
-}
-
-func SetupGlobalLogger(appLogDir string) func() {
-	// Default `stdout` pretty logging
-	stdoutHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: level})
-	
-	// Log filename format
-	format := time.Now().Format("20060102T150405")
-	logFileName := filepath.Join(appLogDir, "awee-"+runtime.GOOS+"-"+format+".log")
-	
-	// File logger setup (JSON format)
-	fileHandler := slog.NewJSONHandler(&lumberjack.Logger{
-		Filename:   logFileName,
-		MaxSize:    2,  // MB
-		MaxBackups: 5,  // Number of backups
-		MaxAge:     28, // Days
-		Compress:   false,
-	}, &slog.HandlerOptions{Level: level})
-	
-	setLogger(slog.New(&MultiHandler{handlers: []slog.Handler{stdoutHandler, fileHandler}}))
-	
-	return func() {
-		Info("Shutting down logging")
-	}
-}
-
-// MultiHandler enables logging to multiple outputs
-type MultiHandler struct {
-	handlers []slog.Handler
-}
-
-func (m *MultiHandler) Enabled(ctx context.Context, level slog.Level) bool {
-	for _, h := range m.handlers {
-		if h.Enabled(ctx, level) {
-			return true
-		}
-	}
-	return false
-}
-
-func (m *MultiHandler) Handle(ctx context.Context, r slog.Record) error {
-	for _, h := range m.handlers {
-		_ = h.Handle(ctx, r) // Ignore errors
-	}
-	return nil
-}
-
-func (m *MultiHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	newHandlers := make([]slog.Handler, len(m.handlers))
-	for i, h := range m.handlers {
-		newHandlers[i] = h.WithAttrs(attrs)
-	}
-	return &MultiHandler{handlers: newHandlers}
-}
-
-func (m *MultiHandler) WithGroup(name string) slog.Handler {
-	newHandlers := make([]slog.Handler, len(m.handlers))
-	for i, h := range m.handlers {
-		newHandlers[i] = h.WithGroup(name)
-	}
-	return &MultiHandler{handlers: newHandlers}
-}
---
+++ multi.go
@@ -0,0 +1,43 @@
+package log
+
+import (
+	"context"
+	"log/slog"
+)
+
+// MultiHandler enables logging to multiple outputs
+type MultiHandler struct {
+	handlers []slog.Handler
+}
+
+func (m *MultiHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	newHandlers := make([]slog.Handler, len(m.handlers))
+	for i, h := range m.handlers {
+		newHandlers[i] = h.WithAttrs(attrs)
+	}
+	return &MultiHandler{handlers: newHandlers}
+}
+
+func (m *MultiHandler) WithGroup(name string) slog.Handler {
+	newHandlers := make([]slog.Handler, len(m.handlers))
+	for i, h := range m.handlers {
+		newHandlers[i] = h.WithGroup(name)
+	}
+	return &MultiHandler{handlers: newHandlers}
+}
+
+func (m *MultiHandler) Enabled(ctx context.Context, level slog.Level) bool {
+	for _, h := range m.handlers {
+		if h.Enabled(ctx, level) {
+			return true
+		}
+	}
+	return false
+}
+
+func (m *MultiHandler) Handle(ctx context.Context, r slog.Record) error {
+	for _, h := range m.handlers {
+		_ = h.Handle(ctx, r) // Ignore errors
+	}
+	return nil
+}
